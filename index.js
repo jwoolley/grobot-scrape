81ee736e0a894ee2b81d5c23dc9d8eaa85f05f0a
Justin Woolley <19597672+jwoolley@users.noreply.github.com>
2018-06-12 00:04:19 -0700

    get threads from index page

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const jar = request.jar();
 
 const homepage = 'http://s6.zetaboards.com/EmpireLost/index/';
 
+// general utility functions
+
+function hash(str) {
+  var hash = 0, i, chr;
+  if (str.length === 0) { 
+    return hash;
+  }
+  for (i = 0; i < str.length; i++) {
+    chr = str.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return btoa(hash);
+}
+
 // request utility functions
 
 function loadCookies(filepath) {
@@ -22,7 +37,8 @@ function setCookies(jar, cookies, url) {
 }
 
 async function getUrl(url) {
-   try {
+  console.log('request: ' + url);
+  try {
     return await request.get({url: url, jar: jar});
   } catch (e) {
     console.log(e);
@@ -47,7 +63,7 @@ function getLinks(html) {
   return links.filter(link => !!link.href).map(link => { return {url: link.href, text: link.innerHTML}; });
 }
 
-async function getSubforumLinks(forumUrl) {
+async function getForumLinks(forumUrl) {
   const html = await getUrl(forumUrl);
   return getLinks(html).filter(link => link.url.match(/^.*\/forum\/.*$/));
 }
@@ -57,8 +73,42 @@ async function getThreadLinks(forumUrl) {
   const forums = getLinks(html).filter(link => link.url.match(/^.*\/topic\/.*$/));
 }
 
+function getForumId(forumUrl) {
+  const match = forumUrl.match(/forum\/(\d+?)\//);
+  return match && match[1] ? match[1] : undefined;
+}
+
+// async function walkThreads(forumUrl, forumName='home', visited={}) {
+//   const forum = { url: forumUrl, name: forumName, subforums: {} };
+  
+//   try {
+//     const subforums = await getForumLinks(forumUrl);
+//     subforums.filter(subforum => !visited[getForumId(subforum.url)]).forEach(async subforum => { 
+//       console.log(`${forumName} subforum:`, subforum);
+//       const subsubforums = await walkThreads(subforum.url, subforum.text, visited);
+//       Object.assign(forum.subforums, subsubforums);
+//       Object.assign(visited, subsubforums.visited);
+//       console.log('subforums:', subsubforums);
+//       console.log(visited, visited);
+//     });
+
+//     visited[getForumId(forumUrl)] = forum;
+//   } catch (e) {
+//     console.warn(e);
+//   }
+//   return forum;
+// }
 
 // scraping logic
+
+async function getForums(forumUrl) {
+  const forum = { url: forumUrl, name: 'home', subforums: [] };
+  
+  const subforums = await getForumLinks(forumUrl);
+  subforums.forEach(subforum => forum.subforums.push({ id: getForumId(subforum.url), url: subforum.url, name: subforum.text }));
+
+  return forum;
+}
 
 if (!process.argv[2]) {
   console.log('usage: node index.js [path-to-cookie]');
@@ -66,11 +116,9 @@ if (!process.argv[2]) {
 }
 const cookiePath = process.argv[2];
 
-const visited = {};
-
 setCookies(jar, loadCookies(cookiePath), homepage);
-
+   
 (async () => {
-  var forums = await getSubforumLinks(homepage);
-  forums.forEach(forum => { console.log(encoder.htmlDecode(forum.text));} );
+  const forums = await getForums(homepage);
+  console.log('Forums:\n', forums);
 })();
